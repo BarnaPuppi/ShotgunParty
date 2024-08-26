bf1602bb6d80d4145229e97ad180753f534b9dd8
Balki22 <123730550+Balki22@users.noreply.github.com>
2024-08-26 20:08:34 +0200

    Added background

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    private GameObject bg;
+    private List<PlayerSystemManager.Player> playerList;
+    private float maxDistance = 4;
+
+    void Start() {
+        bg = this.gameObject;
+        playerList = GameObject.Find("PlayerSystemManager").GetComponent<PlayerSystemManager>().playerList;
+    }
+
+    void Update() {
+        Vector3 avgPos = Vector3.zero;
+        int playerCount = 0;
+        foreach (PlayerSystemManager.Player player in playerList) {
+            if (player.lifeCount > 0) {
+                avgPos += player.gameObject.transform.position;
+                playerCount++;
+            }
+        }
+        avgPos /= (playerCount + 1);
+        bg.transform.position = Vector3.ClampMagnitude(avgPos, maxDistance) * 0.3f;
+    }
+}
